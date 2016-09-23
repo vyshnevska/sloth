365c9032681ca087aef7ff95b8871d192c24ca3f
vyshnevska <vyshnevska.n@gmail.com>
2016-09-23 17:02:26 +0200

    Seed db with static data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+print '.' if Crumb.create(field_key: 'github_commits', number: 1552, label: 'Contribution in Github', visible: true)
+print '.' if Crumb.create(field_key: 'github_inserts', number: 67999, label: 'Added in total lines of code', visible: true)
+print '.' if Crumb.create(field_key: 'github_deletions', number: 52848, label: 'Deleted lines of code', visible: true)
+print '.' if Crumb.create(field_key: 'first_commit', number: 1, label: 'On Jun 30, 2015, commit hash c5d0fb53017d4f47ef258e956d62460f6bfa5123', visible: true)
+
+working_days = (Date.parse('2016-09-23') - Date.parse('2015-06-30')).to_i
+print '.' if Crumb.create(field_key: 'working_days', number: working_days, label: 'Days at Makerist', visible: true)
