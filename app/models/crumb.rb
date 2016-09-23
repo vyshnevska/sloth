0b86534cff46fff56939d3af3c25a8994b7b7d91
vyshnevska <vyshnevska.n@gmail.com>
2016-09-23 17:01:57 +0200

    Odometer js lib for representing crumbs

--- app/models/crumb.rb
+++ app/models/crumb.rb
@@ -1,2 +1,7 @@
 class Crumb < ActiveRecord::Base
+  validates :label, presence: true
+  validates :number, presence: true
+  validates :field_key, uniqueness: true, presence: true
+
+  scope :visible, lambda { where(visible: true) }
 end
